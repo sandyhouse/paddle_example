95caa5c3f521f84290d6420ba9fa95621e5eee53
Li, Long <lilong12@baidu.com>
2019-04-25 17:25:44 +0800

    add example_datasets.py

---
+++ example_datasets.py
@@ -0,0 +1,20 @@
+"""
+This file shows the usage of paddle datasets.
+"""
+
+import paddle.datasets.cifar as cifar
+
+if __name__ == '__main__':
+    print(">>>CIFAR DATA")
+    print("\ttrain100")
+    for data, label in cifar.train100():
+        print("label: {}".format(label))
+    print("\ttest100")
+    for data, label in cifar.test100():
+        print("label: {}".format(label))
+    print("\ttrain10")
+    for data, label in cifar.train10():
+        print("label: {}".format(label))
+    print("\ttest10")
+    for data, label in cifar.test10():
+        print("label: {}".format(label))
