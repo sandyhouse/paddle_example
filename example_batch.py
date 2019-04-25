2814f397b5da0a3bac8275d1006eed75bd6bcb27
Li, Long <lilong12@baidu.com>
2019-04-25 15:04:19 +0800

    add the example on how to use paddle.batch

---
+++ example_batch.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+import paddle.batch as batch
+
+def simple_reader():
+    for i in xrange(105):
+        yield i
+
+if __name__ == '__main__':
+    print("Testing for drop_last=False")
+    reader = batch(simple_reader, 10, drop_last=False)
+    for i, data in enumerate(reader()):
+        print("{}: {}".format(i, data))
+
+    print("Testing for drop_last=True")
+    reader = batch(simple_reader, 10, drop_last=True)
+    for i, data in enumerate(reader()):
+        print("{}: {}".format(i, data))
