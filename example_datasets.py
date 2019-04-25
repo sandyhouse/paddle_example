b4b45ad1388ca151e0d431a16b267a6da278712d
Li, Long <lilong12@baidu.com>
2019-04-25 17:29:22 +0800

    fix a typo bug

--- example_datasets.py
+++ example_datasets.py
@@ -7,14 +7,5 @@ import paddle.dataset.cifar as cifar
 if __name__ == '__main__':
     print(">>>CIFAR DATA")
     print("\ttrain100")
-    for data, label in cifar.train100():
-        print("label: {}".format(label))
-    print("\ttest100")
-    for data, label in cifar.test100():
-        print("label: {}".format(label))
-    print("\ttrain10")
-    for data, label in cifar.train10():
-        print("label: {}".format(label))
-    print("\ttest10")
-    for data, label in cifar.test10():
-        print("label: {}".format(label))
+    datas, labels = cifar.train100()
+    print("label: {}".format(labels))
